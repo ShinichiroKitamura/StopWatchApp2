b243045c0713a91d0873d9337eecb152dbf44d5f
shinichiro106 <skssikm@gmail.com>
2017-06-24 12:33:08 +0900

    Startボタン押下時に経過時刻を表示

--- StopWatchApp2/ViewController.swift
+++ StopWatchApp2/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
     
+    var startTime: TimeInterval? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +24,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func update(){
+        
+        //Optional Binding
+        if let startTime = self.startTime {
+            
+            //スタートボタン押下時からの経過時刻を取得
+            let t:Double = Date.timeIntervalSinceReferenceDate - startTime
+            
+            //取得した時刻をラベル表示用定数に設定
+            let min = Int(t / 60)
+            let sec = Int(t) % 60
+            let msec = Int((t - Double(sec)) * 100.0)
+            
+            //時刻をラベルに設定
+            self.timerLabel.text = String(
+                format:"%02d:%02d:%02d"
+                        , min, sec, msec
+            )
+        }
+    }
+    
     @IBAction func startTimer(_ sender: Any) {
+        
+        //起動時間を取得
+        self.startTime = Date.timeIntervalSinceReferenceDate
+        
+        //タイマーを起動
+        Timer.scheduledTimer(
+            timeInterval: 0.01
+            , target: self
+            , selector: #selector(self.update)
+            , userInfo: nil
+            , repeats: true
+        )
     }
 
     
